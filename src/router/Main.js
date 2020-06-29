8a6d3f08a724090a4a806dd6d7d9eeb20110f6fb
히지니 <potato9417@gmail.com>
2020-06-29 12:54:41 +0900

    usestate 사용하여 완성

--- src/router/Main.js
+++ src/router/Main.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Main =()=>{
     return (
-        <h1>MainPage</h1>
+        <h3>HOME</h3>
     )
 }
 
--- src/router/Usestate.js
+++ src/router/Usestate.js
@@ -1,9 +1,20 @@
-import React from "react";
-import useState from 'usestate';
+import React, {useState} from "react";
 
 const Usestate=()=>{
+    const [item, setItem] = useState(1);
+    const plus=()=>{
+        setItem(item+1)
+    }
+    const minus=()=>{
+        setItem(item-1)
+    }
     return (
-        <h1>usestate</h1>
+        <div className="container">
+            <h3>USESTATE</h3>
+            <h4>{item}</h4>
+            <button onClick={plus} className="btns">PLUS</button>
+            <button onClick={minus} className="btns">MINUS</button>
+        </div>
     )
 }
 
