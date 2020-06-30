aec18aa5dd7f842a76905fb1791da02f590097c1
히지니 <potato9417@gmail.com>
2020-06-30 15:22:28 +0900

    usepreventleave 완성

--- src/router/useeffect/Usepreventleave.js
+++ src/router/useeffect/Usepreventleave.js
@@ -1,9 +1,23 @@
 import React from "react"
 
+const usePreventLeave=()=>{
+    const listener=event=>{
+        event.preventDefault()
+        event.returnValue=""
+    }
+    const enablePrevent=()=>{window.addEventListener("beforeunload",listener)}
+    const disenablePrevent=()=>{window.addEventListener("beforeunload",listener)}
+
+    return {enablePrevent,disenablePrevent}
+}
+
 const Usepreventleave=()=>{
+    const {enablePrevent,disenablePrevent} = usePreventLeave()
     return(
         <div className="container">
             <h3>Usepreventleave</h3>
+            <button onClick={enablePrevent}>Protect</button>
+            <button onClick={disenablePrevent}>Unprotect</button>
         </div>
     )
 }
