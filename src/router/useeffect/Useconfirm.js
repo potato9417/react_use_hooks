2d8f4f6c73787fbd90c8f7d53ecc44f449a99c56
히지니 <potato9417@gmail.com>
2020-06-30 12:58:03 +0900

    useconfirm 완성

--- src/router/useeffect/Useconfirm.js
+++ src/router/useeffect/Useconfirm.js
@@ -1,9 +1,32 @@
 import React from "react"
 
+const useConfirm=(message="",callback,rejection)=>{
+    if(typeof callback !=="function"){
+        return;
+    }
+    const confirmAction =()=>{
+        if(window.confirm(message)){
+            callback()
+        }
+        else{
+            rejection()
+        }
+    }
+    return confirmAction
+}
+
 const Useconfirm=()=>{
+    const deleteWorld =()=>{
+        console.log("Deleteing the World..")
+    }
+    const saveWorld =()=>{
+        console.log("Save the World!")
+    }
+    const confirmDelete= useConfirm("Are you sure?",deleteWorld,saveWorld)
     return(
         <div className="container">
             <h3>useconfirm</h3>
+            <button onClick={confirmDelete}>Delete the world</button>
         </div>
     )
 }
