8ba3a91cb3980ad348362f3a73cc366286a28795
히지니 <potato9417@gmail.com>
2020-07-01 15:44:02 +0900

    usdfullscreen finish

--- src/router/useeffect/Usefullscreen.js
+++ src/router/useeffect/Usefullscreen.js
@@ -1,9 +1,31 @@
-import React from "react"
+import React, { useRef } from "react"
+import { elementType } from "prop-types";
+
+const useFullscreen=()=>{
+    const element=useRef();
+    const triggerFull=()=>{
+        if(element.current){
+            element.current.requestFullscreen();
+        }
+    }
+    const exitFull=()=>{
+        document.exitFullscreen()
+    }
+    return {element,triggerFull,exitFull}
+}
 
 const Usefullscreen=()=>{
+    const {element,triggerFull,exitFull} = useFullscreen()
     return(
         <div className="container">
             <h3>Usefullscreen</h3>
+            <img
+                ref={element} 
+                src="https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/4arX/image/okKVeMMUUBAoUYUrEAzKkmY-9KA.jpg" 
+                alt="cat"
+            />
+            <button onClick={triggerFull}>Fullscreen</button>
+            <button onClick={exitFull}>Exit Fullscreen</button>
         </div>
     )
 }
