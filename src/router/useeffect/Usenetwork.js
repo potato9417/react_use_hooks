39193a121f0ca6abaefed65097700f32b62cb7a8
히지니 <potato9417@gmail.com>
2020-07-01 13:05:13 +0900

    usenetwork finish

--- src/router/useeffect/Usenetwork.js
+++ src/router/useeffect/Usenetwork.js
@@ -1,9 +1,29 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+
+const useNetwork=onchange=>{
+    const[status,setStatus] = useState(navigator.onLine)
+    const handleChange=()=>{
+        if(typeof onchange!=="function"){
+            setStatus(navigator.onLine)
+        }
+    }
+    useEffect(()=>{
+        window.addEventListener("online",handleChange)
+        window.addEventListener("offline",handleChange)
+        // ()=>{
+        //     window.removeEventListener("online",handleChange)
+        //     window.removeEventListener("offline",handleChange)
+        // }
+    },[])
+    return status;
+}
 
 const Usenetwork=()=>{
+    const onLine = useNetwork()
     return(
         <div className="container">
             <h3>Usenetwork</h3>
+            <p>{onLine? "Online" : "OffLine"}</p>
         </div>
     )
 }
