beee1b0bf76249c4f173aa1dffa108da7afb3599
히지니 <potato9417@gmail.com>
2020-07-01 15:03:30 +0900

    usescroll finish 및 usenetwork 수정

--- src/router/useeffect/Usenetwork.js
+++ src/router/useeffect/Usenetwork.js
@@ -10,10 +10,10 @@ const useNetwork=onchange=>{
     useEffect(()=>{
         window.addEventListener("online",handleChange)
         window.addEventListener("offline",handleChange)
-        // ()=>{
-        //     window.removeEventListener("online",handleChange)
-        //     window.removeEventListener("offline",handleChange)
-        // }
+        return ()=>{
+            window.removeEventListener("online",handleChange)
+            window.removeEventListener("offline",handleChange)
+        }
     },[])
     return status;
 }
--- src/router/useeffect/Usescroll.js
+++ src/router/useeffect/Usescroll.js
@@ -1,9 +1,24 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+
+const useScroll=()=>{
+    const [state,setState]=useState({x:0,y:0})
+    console.log("red")
+    const onScroll=()=>{
+        console.log("y ",window.scrollY," x ",window.scrollX)
+        setState({x:window.scrollX,y:window.scrollY})
+    }
+    useEffect(()=>{
+        window.addEventListener("scroll",onScroll)
+        return ()=>window.removeEventListener("scroll",onscroll)
+    },[])
+    return state;
+}
 
 const Usescroll=()=>{
+    const {y} =useScroll()
     return(
-        <div className="container">
-            <h3>Usescroll</h3>
+        <div className="container" style={{height:"1000vh"}}>
+            <h3 style={{position:"fixed", color: y>100 ? "red":"blue" }}>Usescroll</h3>
         </div>
     )
 }
