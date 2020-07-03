dee65a489a1136be0a98313e6d1f8679ad6efd72
히지니 <potato9417@gmail.com>
2020-07-03 13:14:51 +0900

    조건부 오류 자꾸남 이거 ㅇ뭐야..

--- src/router/useeffect/Useaxios.js
+++ src/router/useeffect/Useaxios.js
@@ -1,7 +1,33 @@
-import React from "react"
-// import defaultAxios from "axios";
+import React, { useState, useEffect } from "react"
+import defaultAxios from "axios";
+
+const useAxios=(option,axiosInstance=defaultAxios)=>{
+    const [state,setState]=useState({
+        loading:true, error:null, data:null
+    })
+    // console.log("state:  ",state)
+    if(!option.url){
+        return
+    }
+    useEffect(()=>{
+        axiosInstance(option)
+            .then(data=>{
+                setState({
+                    ...state,
+                    loading:false,
+                    data
+                })
+            })
+            .catch(error=>{
+                setState({...state,loading:false,error})
+            })
+    },[])
+    return state
+}
 
 const Useaxios=()=>{
+    const {loading,error,data} = useAxios({url:"https://yts.am.api/v2/list_movies.json"})
+    console.log(`Loading : ${loading}\nError : ${error}\nData : ${data}`)
     return(
         <div className="container">
             <h3>Useaxios</h3>
