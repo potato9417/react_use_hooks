df120ab22cad9d6bd7295a7a8322a63298bffb98
히지니 <potato9417@gmail.com>
2020-07-03 12:52:27 +0900

    usenotification 완료

--- src/router/useeffect/Useaxios.js
+++ src/router/useeffect/Useaxios.js
@@ -1,4 +1,5 @@
 import React from "react"
+// import defaultAxios from "axios";
 
 const Useaxios=()=>{
     return(
--- src/router/useeffect/Usenotification.js
+++ src/router/useeffect/Usenotification.js
@@ -1,11 +1,35 @@
-import React from "react"
+import React, { useEffect } from "react"
+
+const useNotification=(title,options)=>{
+    if(!("Notification" in window)){
+        return;
+    }
+    const fireNotif=()=>{
+        if(Notification.permission!=="granted"){
+            Notification.requestPermission().then(permission =>{
+                if(permission=="granted"){
+                    new Notification(title,options)
+                }
+                else{
+                    return
+                }
+            })
+        }
+        else{
+            new Notification(title,options)
+        }   
+    }
+    return fireNotif
+}
 
 const Usenotification=()=>{
+    const triggerNotif = useNotification("You clicked button!",{body:"clicked button!!!!!!!!!!!"})
     return(
         <div className="container">
             <h3>Usenotification</h3>
+            <button className="btns" onClick={triggerNotif}>Click!</button>
         </div>
     )
 }
-
+// https://developer.mozilla.org/ko/docs/Web/API/notification
 export default Usenotification
